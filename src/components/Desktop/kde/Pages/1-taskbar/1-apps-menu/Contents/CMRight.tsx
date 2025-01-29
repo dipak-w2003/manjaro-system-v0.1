d3356b6d3b3c21a172bc7c5e1939a9d3aaff739c
dipak-w2003 <dipak2003w@gmail.com>
2025-01-29 19:15:08 +0545

    todo Recent Functionality see draw.io

--- src/components/Desktop/kde/Pages/1-taskbar/1-apps-menu/Contents/CMRight.tsx
+++ src/components/Desktop/kde/Pages/1-taskbar/1-apps-menu/Contents/CMRight.tsx
@@ -1,11 +1,17 @@
 import React, { Suspense } from "react";
-import { AppCategory } from "../AppMenuList";
+import { AppCategory, AppLists } from "../AppMenuList";
 import { AiOutlineReload } from "react-icons/ai";
 
 interface IAppCategory {
   appCategory: AppCategory;
 }
 const CMRight: React.FC<IAppCategory> = ({ appCategory }) => {
+  const apps_listByCategory = React.useMemo(
+    () => AppLists.find((e) => e.appCategory === appCategory),
+    [appCategory],
+    // Recalculate only when appCategory changes
+  );
+  console.log(apps_listByCategory.childrens);
   return (
     <div className={`w-[60%] flex justify-center items-center`}>
       <Suspense
